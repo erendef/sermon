db836163809b49ec8179c26ff4ca114a44bec6a8
EREN ÇOLAK <erencol@hotmail.com>
2025-08-04 10:39:33 +0300

    Update Host.java
    Link Update

--- app/src/main/java/com/erencol/sermon/Data/Service/Host.java
+++ app/src/main/java/com/erencol/sermon/Data/Service/Host.java
@@ -1,7 +1,7 @@
 package com.erencol.sermon.Data.Service;
 
 public class Host {
-     public static final String baseUrl = "https://mobizoe.com/wp-content/uploads/2025/07/";
+     public static final String baseUrl = "https://mobizoe.com/wp-content/uploads/2025/08/";
 
      public static final String getSermons = "sermon.json?t="+System.currentTimeMillis();
      // Cache'lemeyi iptal etmek için bu gerekli.
